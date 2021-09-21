18aa11807045a9dd6e8d28c1450a0b8826f5a183
Dmitry Popov <ant1h4t3rz@gmail.com>
2021-09-21 11:31:33 +0900

    Overloaded lt method for Lecturers

--- main.py
+++ main.py
@@ -84,6 +84,20 @@ class Lecturer(Mentor):
                f'Средняя оценка за лекции: {self.avg_grade()}')
         return msg
 
+    def __lt__(self, other):
+        if self.avg_grade() == other.avg_grade():
+            msg = (
+                f'Средние оценки у лекторов {self.name} {self.surname} и {other.name} {other.surname} одинаковы')
+            print(msg)
+        elif self.avg_grade() > other.avg_grade():
+            msg = (
+                    f'Средняя оценка у лектора {self.name} {self.surname} выше, чем у лектора {other.name} {other.surname}')
+            print(msg)
+        else:
+            msg = (
+                    f'Средняя оценка у лектора {other.name} {other.surname} выше, чем у лектора {self.name} {self.surname}')
+            print(msg)
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -113,21 +127,17 @@ class Reviewer(Mentor):
 student_first = Student('My', 'Name', 'Gender')
 lecturer_first = Lecturer('My', 'Lecturer')
 reviewer_first = Reviewer('My', 'Reviewer')
+lecturer_second = Lecturer('Other', 'Lecturer')
 
 student_first.courses_in_progress += ['Python']
 student_first.finished_courses += ['Git']
 lecturer_first.courses_attached += ['Python']
 reviewer_first.courses_attached += ['Python']
-
+lecturer_second.courses_attached += ['Python']
 student_first.rate_lec(lecturer_first, 'Python', 10)
-student_first.rate_lec(lecturer_first, 'Python', 10)
-student_first.rate_lec(lecturer_first, 'Python', 8)
-reviewer_first.rate_student(student_first, 'Python', 10)
-reviewer_first.rate_student(student_first, 'Python', 10)
-reviewer_first.rate_student(student_first, 'Python', 1)
+student_first.rate_lec(lecturer_second, 'Python', 10)
 
-lecturer_first.avg_grade()
-
+print(lecturer_first > lecturer_second)
 print(reviewer_first)
 print(lecturer_first)
 print(student_first)
